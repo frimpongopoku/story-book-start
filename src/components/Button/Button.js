0aa9adb95a4dc3016a7ca0b5efa6ef08db2a9b80
frimpongopoku <pongofrimi@gmail.com>
2021-06-17 17:34:44 +0400

    Set story book boiler plate up, and ready to use :ok_hand: :fire:

---
+++ src/components/Button/Button.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "./Button.css";
+export default class Button extends Component {
+  render() {
+    return <button className="button-default"> Trial Button</button>;
+  }
+}
